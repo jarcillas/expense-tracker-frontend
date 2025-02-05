61f6ecbe63d201bde02e8372e60f4de53f92cce1
Jesreal Arcillas <jesrealarcillas@gmail.com>
2025-02-05 21:37:28 +0800

    fixed styling of Expenses list

--- app/_components/Expense.tsx
+++ app/_components/Expense.tsx
@@ -1,3 +1,5 @@
+import { Geist_Mono } from "next/font/google";
+
 type ExpenseType = {
   amount: number;
   description: string;
@@ -5,11 +7,22 @@ type ExpenseType = {
   category: number;
 };
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+});
+
+const formatter = Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
 const Expense = ({ expense }: { expense: ExpenseType }) => {
   return (
-    <div className="flex flex-row justify-between items-center w-full p-4 rounded-md border-gray-400 border-[1px] hover:bg-gray-900 select-none">
+    <div className="flex flex-row justify-between items-center w-full h-full py-2 px-1 hover:bg-gray-900 select-none">
       <div>{expense.description}</div>
-      <div>{expense.amount}</div>
+      <div className={geistMono.className}>
+        {formatter.format(expense.amount)}
+      </div>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start min-w-[400px]">
-        <div className="flex w-full gap-4 items-center flex-col">
+        <div className="flex w-full items-center flex-col px-1 border-[1px] rounded-md border-gray-600 divide-gray-700 divide-y-[1px]">
           {expensesMocked.map((expenseItem) => (
             <Expense key={expenseItem.id} expense={expenseItem} />
           ))}
